0b4f12c4882b4750b3e6333028a0c05425a7b927
Cleber Chagas <87093270+CleberChagasAlberto@users.noreply.github.com>
2021-07-08 13:55:17 -0300

    Add files via upload

---
+++ aula07_ex013.py
@@ -0,0 +1,11 @@
+print('|===================|')
+print('|Aumento de salários|')
+print('|===================|')
+
+s = float(input('Insira o valor do salário em reais: '))
+a = float(input('Insira a porcentagem do aumento: '))
+
+#p = s + (s * a / 100)
+#Print('O novo valor do salario com aumento é de {} reais'.format, p)
+
+print(s + (s*a/100))
